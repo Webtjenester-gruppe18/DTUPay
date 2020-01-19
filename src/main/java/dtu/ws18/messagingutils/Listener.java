94a6e5af18951ced91390620a21579a22668e73b
Marcus August Christiansen <s175185@student.dtu.dk>
2020-01-19 13:10:01 +0100

    Update Listener.java

--- src/main/java/dtu/ws18/messagingutils/Listener.java
+++ src/main/java/dtu/ws18/messagingutils/Listener.java
@@ -1,16 +1,35 @@
 package dtu.ws18.messagingutils;
 
 import dtu.ws18.models.Event;
+import dtu.ws18.models.EventType;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class Listener {
+
     @RabbitListener(queues = {RabbitMQValues.DTU_SERVICE_QUEUE_NAME})
     public void receiveEvent(Event event) {
 
         System.out.println(event.getType());
+
+        if (event.getType().equals(EventType.MONEY_TRANSFER_SUCCEED)) {
+
+            // return 200 to the rest caller
+
+        }
+        else if (event.getType().equals(EventType.MONEY_TRANSFER_FAILED)) {
+
+            // return failure to the rest caller
+
+        }
+        else if (event.getType().equals(EventType.TOKEN_VALIDATION_FAILED)) {
+
+            // return failure to the rest caller
+
+        }
+
     }
 
 }
